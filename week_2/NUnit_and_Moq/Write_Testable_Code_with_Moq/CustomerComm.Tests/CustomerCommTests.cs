d7ff58a435ffff20435d0d5d32f88d9d2b19f49c
danishnicho7 <danishnicho7@gmail.com>
2025-06-29 23:31:10 +0530

    Initial commit with CustomerComm and NUnit test

---
+++ week_2/NUnit_and_Moq/Write_Testable_Code_with_Moq/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _mockMailSender
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ReturnsTrue_WhenMailIsSent()
+        {
+            bool result = _customerComm.SendMailToCustomer();
+            Assert.IsTrue(result);
+        }
+    }
+}
