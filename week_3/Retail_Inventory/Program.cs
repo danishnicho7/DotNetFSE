a54cc8b09bd5c2e40e733a41964d1b5566bf3213
danishnicho7 <145546601+danishnicho7@users.noreply.github.com>
2025-07-06 22:15:21 +0530

    Add files via upload

---
+++ week_3/Retail_Inventory/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new RetailContext())
+        {
+            context.Database.EnsureCreated(); 
+
+            if (!context.Categories.Any())
+            {
+                var category = new Category { CategoryName = "Electronics" };
+                var product = new Product { Name = "Laptop", StockLevel = 10, Category = category };
+
+                context.Categories.Add(category);
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+
+            foreach (var p in context.Products.Include(p => p.Category))
+            {
+                Console.WriteLine($"{p.Name} - {p.Category.CategoryName} - Stock: {p.StockLevel}");
+            }
+        }
+    }
+}
